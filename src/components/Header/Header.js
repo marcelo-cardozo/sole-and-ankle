8a5f4416044a14fdc23272b09e9192fbf5e59323
Marcelo Cardozo <marcelo.r.cardozo.g@gmail.com>
2021-07-26 23:17:41 -0400

    style header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -27,11 +27,18 @@ const Header = () => {
 };
 
 const MainHeader = styled.div`
-  padding: 0 32px;
+  padding: 24px 32px;
   border-bottom: 1px solid ${COLORS.gray[300]};
+  display: flex;
+  justify-content: center;
+  align-items: baseline;
 `;
 
-const Nav = styled.nav``;
+const Nav = styled.nav`
+  display: flex;
+  gap: 48px;
+  margin: 0 auto;
+`;
 
 const NavLink = styled.a`
   font-size: 1.125rem;
